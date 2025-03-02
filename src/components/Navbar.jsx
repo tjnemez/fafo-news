f8c03d13451f817b270235bfac76959a22a1396d
tjnemez <fusionai@shaw.ca>
2025-03-02 17:12:30 -0600

    update navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,28 @@
+import { useState } from "react";
+
 const Navbar = () => {
+    const [open, setOpen] = useState(false);
     return (
-        <div className="">
+        <div className="w-full h-16 md:h-20 flex items-center justify-between">
             {/* LOGO */}
-            <div className="">
+            <div className="flex items-center gap-4 text-2xl font-bold">
                 <img src="/logo.png" className="w-8 h-8" alt="" /> 
-                <span>fafo news</span>            
+                <span>FAFO News</span>            
             </div>
             {/* MOBILE MENU */}
-            <div className="md:hidden">M</div>
+            <div className="md:hidden">
+                 {/* MOBILE BUTTON */}
+                <div className="cursor-pointer text-4xl" 
+                onClick={() => setOpen( (prev) => !prev)}
+                >
+                {open ? "X" : "☰"}
+               </div> 
+               {/* MOBILE LINK LIST */}
+                <div className='w-full h-screen flex flex-col items-center justify-center'>
+                    menu
+                </div>
+                
+            </div>
             {/* DESKTOP MENU */}
             <div className="hidden md:flex">D</div>
         </div>
